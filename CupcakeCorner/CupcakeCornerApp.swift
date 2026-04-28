84a380025762dd0f14efc2dc0ca8a7dc978f366d
Edwin Cardenas <eacardenase@gmail.com>
2026-04-27 19:36:14 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Edwin Cardenas on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
